3ed659a549f4a85fe6ea8150275ad6ea5d04bc12
Rufaim <pavelrumyancew@yandex.ru>
2017-12-16 14:31:59 +0300

    blobs data generation

---
+++ clustering/data_generator/blobs.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def make_blobs(n_samples=100, centers=3, cluster_std=1.0,
+               center_box=(-10.0, 10.0), shuffle=True, seed=None):
+    """Generate Gaussian blobs for clustering. Each sample is two dimentional for easy plotting.
+    
+    Parameters
+    ----------
+    n_samples : int, optional (default=100)
+        The total number of points equally divided among clusters.
+    centers : int or array of shape [n_centers, 2], optional(default=3)
+        The number of centers to generate, or the fixed center locations.
+    cluster_std : float or sequence of floats, optional (default=1.0)
+        The standard deviation of the clusters.
+    center_box : pair of floats (min, max), optional (default=(-10.0, 10.0))
+        The bounding box for each cluster center when centers are
+        generated at random.
+    shuffle : boolean, optional (default=True)
+        Shuffle the samples.
+    random_state : int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.
+    Returns
+    -------
+    X : array of shape [n_samples, 2]
+        The generated samples.
+    y : array of shape [n_samples]
+        The integer labels for cluster membership of each sample.
+    """
+    np.random.seed(seed)
+
+    if type(centers) is int:
+        centers = np.random.uniform(center_box[0], center_box[1],size=(centers, 2))
+    else:
+        centers = np.array(centers)
+        assert centers.ndim == 2
+        assert centers.shape[1] == 2
+
+    if type(cluster_std) in [int,float]:
+        cluster_std = float(cluster_std)
+        cluster_std = np.ones(len(centers)) * cluster_std
+
+    X,y = [],[]
+    
+    n_centers = centers.shape[0]
+    n_samples_per_center = [int(n_samples // n_centers)] * n_centers
+
+    for i in range(n_samples % n_centers):
+        n_samples_per_center[i] += 1
+
+    for i, (n, std) in enumerate(zip(n_samples_per_center, cluster_std)):
+        X.append(centers[i] + np.random.normal(scale=std,size=(n, 2)))
+        y += [i] * n
+
+    X = np.concatenate(X)
+    y = np.array(y)
+
+    if shuffle:
+        indices = np.arange(n_samples)
+        np.random.shuffle(indices)
+        X = X[indices]
+        y = y[indices]
+
+    return X, y
