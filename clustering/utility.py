ce19c01272e3d1ccf85f64fa65197bfe1e453b8f
Rufaim <pavelrumyancew@yandex.ru>
2017-12-17 17:54:11 +0300

    elementwise euclidian distances beetween two vectors

--- clustering/utility.py
+++ clustering/utility.py
@@ -1,9 +1,15 @@
 import numpy as np
 
 
-def euclidean_distances(X,squared=True):
+def euclidean_distances(X,Y=None,squared=True):
 	X = np.atleast_2d(X)
-	distance = np.sum((X[np.newaxis,:,:]-X[:,np.newaxis,:])**2,axis=2)
+	if Y is None:
+		Y = X.copy()
+	else:
+		assert Y.shape[0] == X.shape[0]
+	distance = np.sum((X[:,np.newaxis,:]-Y[np.newaxis,:])**2,axis=2)
 	distance = np.maximum(0, distance)
-	distance.flat[::distance.shape[0] + 1] = 0.0
+	if Y is None:
+		# make sure distance to self is zero 
+		distance.flat[::distance.shape[0] + 1] = 0.0
 	return distance if squared else np.sqrt(distance)
