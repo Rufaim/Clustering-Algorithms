d371d542d6bef83593b804a8a4e0da91cf8849b2
Rufaim <pavelrumyancew@yandex.ru>
2017-12-24 16:43:53 +0300

    init files

--- clustering/__init__.py
+++ clustering/__init__.py
@@ -1,7 +1,9 @@
 from .affinity_propagation import AffinityPropagation
 from .cluster_plotter import plot_cluster
 from .utility import euclidean_distances
+from . import datasets
 
 __all__ = ["AffinityPropagation",
 			"plot_cluster",
-			"euclidean_distances"]
+			"euclidean_distances",
+			"datasets"]
---
+++ clustering/datasets/__init__.py
@@ -0,0 +1,14 @@
+from .loader import load_aggregation, load_compound, load_pathbased, load_spiral, \
+				load_D31, load_R15, load_Jain_toy_dataset, load_flame,load_birch1, \
+				load_birch2
+
+__all__ = ["load_aggregation",
+			"load_compound",
+			"load_pathbased",
+			"load_spiral",
+			"load_D31",
+			"load_R15",
+			"load_Jain_toy_dataset",
+			"load_flame",
+			"load_birch1",
+			"load_birch2"]
